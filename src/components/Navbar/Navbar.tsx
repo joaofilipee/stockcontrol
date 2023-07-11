ad1634925858f53b139c1b5eb8ca8d676ea7c98a
João Filipe <joao1filipe@outlook.com>
2023-07-11 17:16:53 -0300

    responsivity almost done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,21 +4,28 @@ import { useContext } from "react"
 
 import { NavLink } from 'react-router-dom'
 
+import { AiOutlineClose } from "react-icons/ai"
+
 import { NavbarRefContext } from "../../contexts/NavbarRefContext"
 
 const Navbar = () => {
   const { navbarRef } = useContext(NavbarRefContext)
+
+  const handleCloseMenu = () => {
+    navbarRef!.current.style.display = "none"
+  }
   
   return (
 
     <nav className={styles.navbar} ref={navbarRef}>
+      <AiOutlineClose className={styles.close_menu} onClick={handleCloseMenu}/>
         
       <section className={styles.route_section}>
         <label>
           <span>Products Registration</span>
 
           <div>
-            <NavLink to="/" className={styles.route}>Register</NavLink>
+            <NavLink to="/" className={styles.route} onClick={handleCloseMenu}>Register</NavLink>
           </div>
         </label>
 
@@ -29,11 +36,11 @@ const Navbar = () => {
           <span>Stocks</span>
 
           <div>
-            <NavLink to="/balance" className={styles.route}>Balance</NavLink>
+            <NavLink to="/balance" className={styles.route} onClick={handleCloseMenu}>Balance</NavLink>
           </div>
 
           <div>
-            <NavLink to="/manageproducts" className={styles.route}>Manage Products</NavLink>
+            <NavLink to="/manageproducts" className={styles.route} onClick={handleCloseMenu}>Manage Products</NavLink>
           </div>
           
         </label>
